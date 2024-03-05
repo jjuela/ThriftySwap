9a0b09a02f434e73445e005e7cacceff8ea12d90
Jonnathan Juela <Jonnathanjuela@gmail.com>
2024-03-05 10:58:03 -0500

    Token

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USERNAME = 'OwlGoSCSU@gmail.com'
-    MAIL_PASSWORD = 'rbjb dwxk lqly smlz'
+    MAIL_PASSWORD = 'Token'
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
     MAIL_DEFAULT_SENDER = ('ThriftyOwl', 'OwlGoSCSU@gmail.com')
